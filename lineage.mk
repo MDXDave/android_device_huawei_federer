25b791ac85ab12a70de2757b81ddb4f50b2971c0
Dave <MDXDave@users.noreply.github.com>
2017-09-23 18:22:41 +0200

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -14,22 +14,10 @@
 
 $(call inherit-product, device/huawei/federer/full_federer.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit some common OMNI stuff.
-$(call inherit-product, vendor/cm/config/common_full.mk)
-
-# TWRP
-#PRODUCT_COPY_FILES += \
-#    device/huawei/federer/recovery/etc/twrp.fstab:recovery/root/etc/twrp.fstab
-
-
-PRODUCT_NAME := cm_federer
+PRODUCT_NAME := lineage_federer
 BOARD_VENDOR := huawei
-
-#PRODUCT_BUILD_PROP_OVERRIDES += \
-#		BUILD_FINGERPRINT=HUAWEI/FDR/HWFDR:5.1.1/HuaweiMediaPad/FDR-A01LC170B002:user/release-keys \
-#	    PRIVATE_BUILD_DESC="FDR-user 5.1.1 HuaweiMediaPad C170B002 release-keys" \
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
